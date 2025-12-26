0ec720737753fabad0a73d163b3b22947ae3b0c3
Luis Alfredo <laguirre@quaxar.com>
2025-12-26 12:38:32 -0600

    add project - pagina web

---
+++ projects/primer-pagina-web/src/Component.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+
+export default function Component() {
+    const [text, setText] = useState();
+    const [update, setUpdate] = useState();
+
+    const textOnChange = (event) => {
+        setText(event.target.value)
+    }
+
+    const buttonOnClick = (event) => {
+        setUpdate(text)
+    }
+
+    return (
+        <div>
+            <input type="text" value={text} onChange={textOnChange} />
+            <button onClick={buttonOnClick}>Actualizar</button>
+            <p>Texto input: {text}</p>
+            <p>texto actualizado: {update}</p>
+        </div>
+    )
+}
